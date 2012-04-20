961a578eebc8e405f78babfc3699946ab189e3e8
Ryan Bradley <rbradley@redhat.com>
2012-04-20 15:44:22 -0400

    Replace previous Java classes with those for the kitchensink example.

---
+++ spring-mvc-archetype/src/main/resources/archetype-resources/src/main/java/mvc/MemberController.java
@@ -0,0 +1,45 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.mvc;
+
+ import javax.validation.Valid;
+
+import org.jboss.as.quickstarts.kitchensink.model.Member;
+import org.jboss.as.quickstarts.kitchensink.repo.MemberDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value="/")
+public class MemberController
+{
+    @Autowired
+    private MemberDao memberDao;
+
+    @RequestMapping(method=RequestMethod.GET)
+    public String displaySortedMembers(Model model)
+    {
+        model.addAttribute("newMember", new Member());
+        model.addAttribute("members", memberDao.findAllOrderedByName());
+        return "index";
+    }
+
+    @RequestMapping(method=RequestMethod.POST)
+    public String registerNewMember(@Valid @ModelAttribute("newMember") Member newMember, BindingResult result, Model model)
+    {
+        if (!result.hasErrors()) {
+            memberDao.register(newMember);
+            return "redirect:/";
+        }
+        else {
+            model.addAttribute("members", memberDao.findAllOrderedByName());
+            return "index";
+        }
+    }
+}
---
+++ spring-mvc-archetype/src/main/resources/archetype-resources/src/main/java/repo/MemberDao.java
@@ -0,0 +1,19 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.repo;
+
+import java.util.List;
+
+import org.jboss.as.quickstarts.kitchensink.model.Member;
+
+public interface MemberDao
+{
+    public Member findById(Long id);
+
+    public Member findByEmail(String email);
+
+    public List<Member> findAllOrderedByName();
+
+    public void register(Member member);
+}
